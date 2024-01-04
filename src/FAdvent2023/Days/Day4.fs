e7d74f9e6fada3d2e29a06113ccb60ce631f4e35
Gregory Bint <kabourneeak@gmail.com>
2024-01-03 21:31:07 -0500

    feat: day 4 part 2

--- src/FAdvent2023/Days/Day4.fs
+++ src/FAdvent2023/Days/Day4.fs
@@ -5,8 +5,8 @@ open FAdvent2023.Extensions
 
 type Card =
     { Id: int
-      Winning: int seq
-      Held: int seq }
+      Winning: int Set
+      Held: int Set }
 
 let toCards input =
     let toCard line =
@@ -32,29 +32,49 @@ let toCards input =
             |> Seq.map int
 
         { Id = id
-          Winning = winning
-          Held = held }
+          Winning = winning |> Set.ofSeq
+          Held = held |> Set.ofSeq }
 
     input |> Seq.map toCard
 
 let part1 input =
     let cards = input |> toCards
 
-    let matches =
-        cards
-        |> Seq.map (fun c -> c.Held |> Seq.where (fun h -> Seq.contains h c.Winning) |> Seq.length)
+    let matchesPerCard =
+        cards |> Seq.map (fun c -> Set.intersect c.Held c.Winning |> Set.count)
 
-    let points =
-        matches |> Seq.where (fun m -> m > 0) |> Seq.map (fun m -> 1 <<< (m - 1))
+    let pointsPerCard =
+        matchesPerCard
+        |> Seq.where (fun m -> m > 0)
+        // left-wise bit shift to do power of 2
+        |> Seq.map (fun m -> 1 <<< (m - 1))
 
-    points |> Seq.sum
+    pointsPerCard |> Seq.sum
 
 let part1Runner () =
     let result = readInputFile "day4.txt" |> part1
     printf $"Day 4 Part 1 Answer: {result}\n"
     ()
 
-let part2 input = "n/a"
+let part2 input =
+
+    let cards = input |> toCards
+
+    let matchesPerCard =
+        cards |> Seq.map (fun c -> Set.intersect c.Held c.Winning |> Set.count)
+
+    // start with count = 1 for each card
+    let countsPerCard = Array.create (Seq.length cards) 1
+
+    matchesPerCard
+    // for each card (by index)
+    |> Seq.iteri (fun i m ->
+        // simulate the generation of cards by updating the apparent count of each card
+        for j in (i + 1) .. (i + m) do
+            // based on how many of the current card have already been generated
+            countsPerCard[j] <- countsPerCard[j] + countsPerCard[i])
+
+    Array.sum countsPerCard
 
 let part2Runner () =
     let result = readInputFile "day4.txt" |> part2
